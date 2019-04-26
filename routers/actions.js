6d1c984b5e6f3886c465ef6a8bf3174ae94e62cf
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-04-26 11:01:16 -0500

    half of endpoints working

--- routers/actions.js
+++ routers/actions.js
@@ -1,20 +1,79 @@
 const express = require('express');
+const db = require('../data/helpers/actionModel');
 
 const actionsRouter = express.Router();
 
-actionsRouter.get('/',(req, res) => {
+actionsRouter.get('/', (req, res) => {
+    db.get()
+        .then(dbRes => {
+            res.send({
+                data: dbRes
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: '500 - Internal Server Error'
+            });
+        });
+});
+
+actionsRouter.get('/:id', (req, res) => {
+    db.get(req.params.id)
+        .then(dbRes => {
+            if (dbRes)
+                res.status(200).send({
+                    data: dbRes
+                });
+            else
+                res.status(422).send({
+                    message: '422 - Invalid ID'
+                });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: '500 - Internal Server Error'
+            });
+        });
+});
+
+actionsRouter.post('/', (req, res) => {
+    try {
+        //Validation
+        if (req.body.project_id === undefined)
+            res.status(422).send({
+                message: '422 - No Project ID'
+            });
+        if (req.body.description === undefined)
+            res.status(422).send({
+                message: '422 - No Action Description'
+            });
+        if (req.body.description > 128)
+            res.status(422).send({
+                message: '422 - Action Description Too Long - 128 Char Limit'
+            });
+        if (req.body.notes === undefined)
+            res.status(422).send({
+                message: '422 - No Action Notes'
+            });
+
+        db.insert(req.body)
+            .then(dbRes => {
+                res.status(200).send({
+                    data: dbRes
+                });
+            });
+    } catch (err) {
+        res.status(500).send({
+            message: '500 - Internal Server Error'
+        });
+    }
+});
+
+actionsRouter.put('/', (req, res) => {
     res.send('Actions here');
 });
 
-actionsRouter.post('/',(req, res) => {
-    res.send('Actions here');
-});
-
-actionsRouter.put('/',(req, res) => {
-    res.send('Actions here');
-});
-
-actionsRouter.delete('/',(req, res) => {
+actionsRouter.delete('/', (req, res) => {
     res.send('Actions here');
 });
 
--- routers/projects.js
+++ routers/projects.js
@@ -1,20 +1,71 @@
 const express = require('express');
+const db = require('../data/helpers/projectModel');
 
 const projectsRouter = express.Router();
 
-projectsRouter.get('/',(req, res) => {
+projectsRouter.get('/', (req, res) => {
+    db.get()
+        .then(dbRes => {
+            res.status(200).send({
+                data: dbRes
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: '500 - Internal Server Error'
+            });
+        });
+});
+
+projectsRouter.get('/:id', (req, res) => {
+    db.get(req.params.id)
+        .then(dbRes => {
+            if (dbRes)
+                res.status(200).send({
+                    data: dbRes
+                });
+            else
+                res.status(422).send({
+                    message: '422 - Invalid ID'
+                });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: '500 - Internal Server Error'
+            });
+        });
+});
+
+projectsRouter.post('/', (req, res) => {
+    try {
+        //Validation
+        if (req.body.name === undefined)
+            res.status(422).send({
+                message: '422 - No Project Name'
+            });
+        if (req.body.description === undefined)
+            res.status(422).send({
+                message: '422 - No Project Description'
+            });
+
+        db.insert(req.body)
+            .then(dbRes => {
+                res.status(200).send({
+                    data: dbRes
+                });
+            });
+    } catch (err) {
+        res.status(500).send({
+            message: '500 - Internal Server Error'
+        });
+    }
+});
+
+projectsRouter.put('/', (req, res) => {
     res.send('Projects here');
 });
 
-projectsRouter.post('/',(req, res) => {
-    res.send('Projects here');
-});
-
-projectsRouter.put('/',(req, res) => {
-    res.send('Projects here');
-});
-
-projectsRouter.delete('/',(req, res) => {
+projectsRouter.delete('/', (req, res) => {
     res.send('Projects here');
 });
 
